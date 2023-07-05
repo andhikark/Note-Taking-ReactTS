687b3b0629455b02ef3e6be3a2778c726e13cf61
Andhika_RK <andhikarestu873@gmail.com>
2023-07-05 10:03:20 +0800

    finishing car card

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,18 @@
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+  
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
+
 export async function fetchCars(){
     const headers = {
         'X-RapidAPI-Key': '56163eff12msh71ad6d88cbc4ce6p17a2dbjsn4ae0b41f38d3',
@@ -12,3 +27,4 @@ export async function fetchCars(){
 
     return result;
 }
+
