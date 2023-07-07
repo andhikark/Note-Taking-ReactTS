d62d25c798c6854073f1057cb45b7b64a689b6e7
Andhika_RK <andhikarestu873@gmail.com>
2023-07-07 23:08:54 +0800

    custom filters

--- utils/index.ts
+++ utils/index.ts
@@ -47,3 +47,13 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
     return `${url}`;
   } 
 
+export const updateSearchParam = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+  const newPathName = `${window.location.pathname}?${searchParams.toString()}`;
+
+  return newPathName
+}
+
